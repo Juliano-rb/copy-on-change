49303190b32f24d99fdff9e8bd0c8e8b9be3857e
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-06-17 19:26:46 -0300

    initial version

---
+++ index.js
@@ -0,0 +1,15 @@
+const readline = require('readline-sync')
+const fs = require('fs');
+
+const fileToWatch = readline.question('File to watch changes: ')
+const copyTo = readline.question('Folder to past file to: ')
+
+console.log(`Watching for file changes on ${fileToWatch}`);
+
+fs.watch(fileToWatch, (event, filename) => {
+  if (filename) {
+    console.log(`${filename} file Changed, copying file to ${copyTo}`);
+
+    fs.copyFileSync(fileToWatch,`${copyTo}/${filename}`)
+  }
+});
